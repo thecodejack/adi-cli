a699519395a6a7156edc5adf24bf5430d12a0d36
Adi <adi.srikanth@adap.tv>
2015-06-22 23:27:03 +0530

    Code Push

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# adi-cli
+adi-cli
+=======
+
+This is my personal CLI which i have been using for months. I have removed most of things and shared my CLI as a template to build your own. Now you can do whatever you want with the code. This is build in Node using Meow, Chalk, Inquirerjs, unirest.
+
+## Install
+
+Makesure node or iojs, npm are installed in your PC.
+Move to the repository folder through terminal
+Run following command
+```
+$ npm install . -g
+```
+
+Please note that first line of file adi.js 
+```
+#!/usr/bin/env iojs
+```
+should be corrected to following if you are using node rather iojs
+```
+#!/usr/bin/env node
+```
+
+## Usage
+Following command display the current forecast of Hyderabad, India.(Will soon moveit to setup)
+```
+$ adi climate
+```
+
+If you run `adi` with any words, it will display information about same
+```
+$ adi Java script
+```
+
+You can add as many commands as you want.
+
+## Support (OS Terminals)
+
+You should expect mostly good support for the CLI below.
+
+- **Mac OS**:
+  - Terminal.app
+  - iTerm
+- **Windows**:
+  - cmd.exe
+  - Powershell
+  - Cygwin
+- **Ubuntu**:
+  - Terminal
+
+## TODO
+There are more commands like `live f1`, `live football`, `live cricket`, `mytwitter`, `hn`(Hacker News). Will push them by replacing personal API keys. 
+
+## License
+
+WFTPL
---
+++ adi.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env iojs
+
+'use strict';
+var meow = require('meow');
+var pkg = require('./package.json');
+var chalk = require('chalk');
+var inquirer = require("inquirer");
+var unirest = require('unirest');
+
+var cli = meow({
+    help: false,
+    pkg: pkg
+});
+
+var opts = cli.flags; //flags
+var args = cli.input; //commands
+var cmd = args[0]; //first command after adi
+console.log(''); //new line
+if (cmd == 'hello') {
+    console.log(chalk.red('Hi There'));
+} else if (cmd == 'askme') {
+    console.log(chalk.red('Hi There. This is Adi.'));
+    var questions = [{
+        type: "input",
+        name: "name",
+        message: "What's your name?",
+        validate: function(input) {
+            return !!input;
+        }
+    }, {
+        type: "confirm",
+        name: "jsboolean",
+        message: "Do you know Javascript?"
+    }];
+
+    inquirer.prompt(questions, function(answers) {
+        console.log(JSON.stringify(answers, null, "  "));
+    });
+} else if (cmd == 'climate') {
+    unirest.get("https://simple-weather.p.mashape.com/weatherdata?lat=17.483473%C2%B0N&lng=78.317731%C2%B0E")
+        .header("X-Mashape-Key", "RMsnGCqBHVmshXsxvelFnbQaASqap1S21x0jsnbKmYuFVnje0S")
+        .header("Accept", "application/json")
+        .end(function(result) {
+            if (result.status == 200) {
+                var report = JSON.parse(result.body).query.results.channel;
+                var lastBuild = report.lastBuildDate;
+                var title = report.item.title;
+                var forecasts = report.item.forecast;
+                console.log(chalk.styles.bold.open + chalk.yellow(title) + chalk.styles.bold.close);
+
+                forecasts.forEach(function(item) {
+                    console.log(chalk.styles.bold.open + chalk.cyan(item.date + '/' + item.day + ': ') + chalk.styles.bold.close +
+                        chalk.magenta(item.text));
+                });
+            } else {
+                console.log(chalk.red('!!!Error Loading data'));
+            }
+        });
+} else {
+	cmd = args.join(' ');
+    unirest.get("http://api.duckduckgo.com/?q='" + cmd + "'&format=json")
+        .end(function(result) {
+        	var body = JSON.parse(result.body);
+        	if (result.status == 200) {
+        		if (body.Abstract !== '') {
+        			console.log(chalk.magenta(body.Abstract));
+		        	console.log('More Details at ' + chalk.blue(body.AbstractURL));
+		        	console.log('Source: ' + chalk.magenta(body.AbstractSource));
+		        	console.log('');
+        		} else {
+        			console.log(chalk.red('Multiple or No Results'));
+        			console.log('Related Data:');
+        			console.log('');
+        			body.RelatedTopics.forEach(function(item){
+        				if(!item.Topics || !item.Topics.length) {
+        					console.log(chalk.magenta(item.Text));
+        					console.log('More Details at ' + chalk.blue(item.FirstURL));
+		        			console.log('-----');
+        				}
+        			});
+        			console.log('');
+        		}
+        	} else {
+        		console.log(chalk.red('!!!Error Loading data'));
+        	}
+        });
+}
